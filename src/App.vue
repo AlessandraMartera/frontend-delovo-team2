20fad85c4823fe152386d8dd2d54d70657b6a147
alessandra martera <ale.martera@gmail.com>
2023-09-05 17:16:43 +0200

    *

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script>
 import axios from 'axios';
-<<<<<<< HEAD
-=======
-
->>>>>>> babad2efa1b0e4ca00e761643db6fd2efc67e6a8
 import AppHeader from './components/AppHeader.vue'
 import RestaurantShow from './components/RestaurantShow.vue'
 
