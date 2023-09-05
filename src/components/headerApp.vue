0187371dafd1f2bf79ad52e181ac17c22ca8fd4a
MirkoGLostia <giggi.torre@mail.com>
2023-09-05 16:39:32 +0200

    creation header connection

---
+++ src/components/headerApp.vue
@@ -0,0 +1,21 @@
+<script>
+// import axios from 'axios';
+
+export default {
+    name: 'headerApp',
+}
+</script>
+
+<template>
+    <h1>
+        Hello from header
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+    padding: 50px 200px;
+    background-color: aquamarine;
+}
+</style>
