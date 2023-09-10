a76fae6e4a8c3039f6d4004d6bc2a92277924836
MirkoGLostia <giggi.torre@mail.com>
2023-09-10 21:01:21 +0200

    update

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,11 @@ export default {
     components: {
         AppSectionTypology
     },
+    data() {
+        return {
+            restaurants: []
+        }
+    },
     mounted() {
 
         axios.get('http://127.0.0.1:8000/api/restaurants')
