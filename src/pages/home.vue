4a9bc10f876b06d69a8a2a7c97d513dd76df4412
alessandra martera <ale.martera@gmail.com>
2023-09-11 19:09:15 +0200

    update  getTypology(id) refacoring

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,34 +9,47 @@ export default {
     data() {
         return {
             restaurants: [],
-            typologyId: [],
+            typologies: [],
+            selectedTypologies: [],
         }
     },
     methods: {
         getTypology(id) {
-            this.typologyId = id;
-            console.log(this.typologyId);
 
+            // per mettere id selezionato nell'arr degli id selezionati
+            // controlle se l'id è già presente nel selectedTypology
+            if (this.selectedTypologies.includes(id)) {
+
+                // se si lo elimino
+                Array.prototype.remove = function (value) {
+                    this.splice(this.indexOf(value), 1);
+                }
+                this.selectedTypologies.remove(id);
+            } else {
+                // se non è persente nel selectTypology lo inserisco
+                this.selectedTypologies.push(id)
+            }
+
+            // BOH
             axios.get('http://127.0.0.1:8000/api/restaurants')
                 .then(res => {
-
                     this.restaurants = res.data.restaurants;
-
+                    this.restaurants.forEach(restaurant => {
+                        this.typologies = restaurant.typologies;
+                    })
                 }).catch(error => {
                     console.log(error);
                 })
         }
     },
-    // mounted() {
-    //     axios.get('http://127.0.0.1:8000/api/restaurants')
-    //         .then(res => {
-
-    //             this.restaurants = res.data.restaurants;
-
-    //         }).catch(error => {
-    //             console.log(error);
-    //         })
-    // }
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/restaurants')
+            .then(res => {
+                this.restaurants = res.data.restaurants;
+            }).catch(error => {
+                console.log(error);
+            });
+    }
 }
 
 </script>
@@ -45,14 +58,28 @@ export default {
     <AppSectionTypology @search="getTypology" />
 
     <div class="my-4">
-        <ul>
-            <li v-for="(restaurant, idx) in this.restaurants">
-                <router-link :to="{ name: 'about', params: { id: restaurant.id } }">
-                    {{ restaurant.nome }}
-                </router-link>
+        <div v-if="selectedTypologies.length === 0">
 
-            </li>
-        </ul>
+            <div v-for="(restaurant, idx) in this.restaurants">
+                {{ restaurant.nome }}
+            </div>
+        </div>
+
+        <div v-else>
+            <div v-for="(restaurant, idx) in this.restaurants">
+
+                <div v-for="typology in restaurant.typologies">
+
+
+                    <div v-if="typology.id === this.selectedTypologies">
+                        <router-link :to="{ name: 'about', params: { id: restaurant.id } }">
+                            {{ restaurant.name }}
+                        </router-link>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
