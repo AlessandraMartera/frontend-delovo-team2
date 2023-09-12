322141821686ceabaaf308264bc15fb41ff5d57c
alessandra martera <ale.martera@gmail.com>
2023-09-12 12:08:12 +0200

    print product in show restaurant

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -11,6 +11,7 @@ export default {
     axios.get(`http://127.0.0.1:8000/api/show-restaurant/${this.$route.params.id}`)
       .then((response) => {
         this.restaurant = response.data.restaurant;
+        // console.log(this.restaurant);
       })
   },
 };
@@ -28,7 +29,15 @@ export default {
     </h1>
     <div>ci puoi trovare {{ restaurant.indirizzo }}</div>
     <div>
-      {{ restaurant.products }}
+      <h3>
+        questo è il nostro menu
+      </h3>
+      <ul>
+        <li v-for="product in restaurant.products">
+          {{ product.nome }}
+        </li>
+      </ul>
+
     </div>
   </div>
 </template>
