01a8297fac626ae23f62ecd7e13193a64619f66b
FedericaMammone <federicakr97@gmail.com>
2023-09-18 23:22:43 +0200

    responsivity done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -29,55 +29,55 @@ export default {
 
 <template>
   <header>
-    <nav class="background-container navbar navbar-expand-lg navbar-light bg-light align-items-center">
-      <div class="col-lg-2 col-sm-8">
+    <nav class="background-container">
+      <div class="container-header">
         <div id="logo">
-          <img src="./../assets/images/Logo3.svg" alt="" />
+          <router-link :to="{ name: 'home' }">
+            <img src="./../assets/images/Logo3.svg" alt="" />
+          </router-link>
         </div>
-      </div>
 
-      <div class="header-menu-desktop col-lg-10">
-        <div class="col-2 d-flex align-items-center">
-          <router-link class="home" :to="{ name: 'home' }">Torna alla Home</router-link>
-        </div>
-        <div class="offset-3 col-2 d-flex gap-4 align-items-center">
+        <div class="header-menu-desktop ">
           <a href="http://127.0.0.1:8000/register">
             <button>Registrati</button>
           </a>
           <a href="http://127.0.0.1:8000/login">
             <button>Accedi</button>
           </a>
+
           <button @click="
             this.$store.state.cart_visible = !this.$store.state.cart_visible
             ">
             Carrello
           </button>
         </div>
-      </div>
 
-      <div class="header-menu-cell col-sm-2">
-        <div class="select d-flex gap-3" @click="showDropdown">
-          <button class="py-0 px-2 rounded">&#11167;</button>
-        </div>
 
-        <div v-if="showmenu">
+        <div class="header-menu-cell">
+          <div @click="showDropdown">
+            <button class="rounded">&#11167;</button>
+          </div>
 
-          <div>
-            <router-link class="home" :to="{ name: 'home' }">Torna alla Home</router-link>
-            <a href="http://127.0.0.1:8000/register">
-              <button>Registrati</button>
-            </a>
-            <a href="http://127.0.0.1:8000/login">
-              <button>Accedi</button>
-            </a>
-            <button @click="
-              this.$store.state.cart_visible = !this.$store.state.cart_visible
-              ">
-              Carrello
-            </button>
+          <div v-if="showmenu">
+
+            <div>
+
+              <a href="http://127.0.0.1:8000/register">
+                <button>Registrati</button>
+              </a>
+              <a href="http://127.0.0.1:8000/login">
+                <button>Accedi</button>
+              </a>
+              <button @click="
+                this.$store.state.cart_visible = !this.$store.state.cart_visible
+                ">
+                Carrello
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
     </nav>
   </header>
 </template>
@@ -93,24 +93,16 @@ header {
   margin-left: -5%;
   padding: 40px 0px;
 
-  button,
-  .home {
-    font-size: 1.5rem;
-    background: #e0cfb5;
-    color: #982931;
-    margin-bottom: 40px;
-    border-radius: 10px;
-    padding: 0 10px;
-  }
-
   .background-container {
+    display: flex;
+    margin-top: 10px;
+
     background-image: url("./../assets/images/Sfondo_sezione_tipologie.jpg");
     background-size: cover;
     background-position: bottom;
     height: 145px;
 
     // box-shadow: 10px 10px 5px black;
-
     // -webkit-box-shadow: -1px 0px 20px 8px #000000;
     // -moz-box-shadow: -1px 0px 20px 8px #000000;
     // -o-box-shadow: -1px 0px 20px 8px #000000;
@@ -120,6 +112,25 @@ header {
     -moz-box-shadow: 10px 11px 24px 6px #000000;
     -o-box-shadow: 10px 11px 24px 6px #000000;
     box-shadow: 10px 11px 24px 6px #000000;
+
+    .container-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 80%;
+      margin: 0 auto;
+
+
+    }
+  }
+
+  button {
+    font-size: 1.5rem;
+    background: #e0cfb5;
+    color: #982931;
+    margin: 5px;
+    border-radius: 10px;
+    padding: 0 20px;
   }
 
 }
@@ -129,10 +140,10 @@ header {
   width: 150px;
   display: flex;
   justify-content: center;
-  margin-left: 200px;
+
   background-color: #992631;
-  margin: 30px 0 0 100px;
-  margin-left: 40%;
+
+
   border-bottom-left-radius: 50%;
   border-bottom-right-radius: 50%;
   border: 5px solid black;
@@ -152,11 +163,8 @@ a {
 }
 
 
-// mediaquery
+// MEDIAQUERY 
 
-.header-menu-desktop {
-  display: flex;
-}
 
 .header-menu-cell {
   display: none;
@@ -164,7 +172,7 @@ a {
 
 
 // visuale cellulare
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 900px) {
 
   .header-menu-desktop {
     display: none;
@@ -173,7 +181,7 @@ a {
   .header-menu-cell {
     display: block;
     z-index: 20;
-    margin-right: 120px;
+
 
     div {
 
@@ -181,19 +189,16 @@ a {
         display: flex;
         flex-direction: column;
         position: absolute;
-        top: 120px;
-        background-image: url("./../assets/images/Sfondo_sezione_tipologie.jpg");
+        background-image: url(/src/assets/images/Sfondo_sezione_tipologie.jpg);
         background-size: cover;
         background-position: bottom;
-        padding: 20px;
+        padding: 10px 15px;
+        right: 124px;
       }
     }
   }
 
-  #logo {
-    margin-left: 20%;
-    margin-top: -20px;
-  }
+
 
 }
 </style>
