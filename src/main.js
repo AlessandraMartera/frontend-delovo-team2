65b5a4e629af4d8dd837e11ec3cb66dd73c5debf
GianbaBotassi <gianba89@gmail.com>
2023-09-19 12:47:08 +0200

    refactor cart

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import { createStore } from 'vuex'
 import { router } from "./router.js"
 
 // import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 // Create a new store instance.
 const store = createStore({
@@ -18,7 +19,10 @@ const store = createStore({
       beUrl: "http://127.0.0.1:8000/storage/",
       items: [],
       cart_visible: false,
-      total: 0
+      total: 0,
+      emptyCart: false,
+      ristoranteDiverso: false,
+      prodottoDiverso: false
     }
   }
 })
